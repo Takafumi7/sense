bed57395bbc6952114d43b8d5b1ccbb18ba4c3a7
Takafumi <Takafumi@T-Mac-Pro.local>
2016-08-08 22:27:07 +0900

    end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 before_action :move_to_index, except: :index
 
 def index
-@articles = Article.order("created_at DESC").page(params[:page]).per(5)
+@articles = Article.includes(:user).order("created_at DESC").page(params[:page]).per(5)
 end
 
 def new
@@ -15,7 +15,7 @@ end
 
 private
 def article_params
-  params.permit(:user_id, :image , :text)
+  params.permit(:image , :text)
 end
 
 def move_to_index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
 def show
  @name = current_user.name
- @articles = Article.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+ @articles = current_user.articles.page(params[:page]).per(5).order("created_at DESC")
 end
 
 end
