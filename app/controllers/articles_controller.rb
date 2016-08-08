818e6223e67283f9eb187b4853a67eff670033f9
Takafumi <Takafumi@T-Mac-Pro.local>
2016-08-08 20:11:55 +0900

    devise compe!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+before_action :move_to_index, except: :index
+
 def index
 @articles = Article.order("created_at DESC").page(params[:page]).per(5)
 end
@@ -16,5 +18,9 @@ def article_params
   params.permit(:user_id, :image , :text)
 end
 
+def move_to_index
+  redirect_to action:  :index unless user_signed_in?
+
+end
 
 end
