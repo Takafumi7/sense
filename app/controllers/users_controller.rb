95d7dbe6d4459b4ac54bd6a649bc306cbf96589f
Takafumi <Takafumi@T-Mac-Pro.local>
2016-08-09 15:41:45 +0900

    make sense!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
 
 def show
+user = User.find(params[:id])
  @name = current_user.name
- @articles = current_user.articles.page(params[:page]).per(5).order("created_at DESC")
+ @articles = user.articles.page(params[:page]).per(5).order("created_at DESC")
 end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808105520) do
+ActiveRecord::Schema.define(version: 20160809015932) do
 
   create_table "articles", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20160808105520) do
     t.text     "image",      limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "article_id", limit: 4
+    t.text     "text",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "users", force: :cascade do |t|
