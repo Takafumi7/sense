b796c275c58a76932a80c3dbd897ef790337f69d
Takafumi <Takafumi@T-Mac-Pro.local>
2016-08-09 17:32:33 +0900

    final!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
 
-def show
-user = User.find(params[:id])
- @name = current_user.name
- @articles = user.articles.page(params[:page]).per(5).order("created_at DESC")
-end
-
-end
+    def show
+    user = User.find(params[:id])
+     @name = user.name
+     @articles = user.articles.page(params[:page]).per(5).order("created_at DESC")
+    end
+ end
